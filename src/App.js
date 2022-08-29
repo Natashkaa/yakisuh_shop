ca90e6a1f6b5dbe8b148d6aca97e92a6d4993452
Natashkaa <natasha.cat982404@gmail.com>
2022-08-29 18:05:53 +0300

    Created Header Component and Simple hyml version of header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Header from './Components/Header/header';
 
 function App() {
-  return <h1>Hello world!</h1>
+  return (
+    <div className='app container-fluid m-0 p-0'>
+      <Header/>
+    </div>
+  )
 }
 
 export default App;
