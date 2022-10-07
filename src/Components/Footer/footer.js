b82e3559f897b49072c569b857da18bf96b27d67
Natashkaa <natasha.cat982404@gmail.com>
2022-10-07 16:44:01 +0300

    Footer with not complete links

---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,20 @@
+import "./footer.css";
+
+const Footer = () => {
+    return( 
+        <footer className="footer">
+            <div className="footer-component">
+                2022
+                </div>
+            <div className="footer-component">
+                <a href="#">Ordering/Shipping Policy</a>
+            </div>
+            <div className="footer-component">
+                <a href="https://www.instagram.com/yakisuh/" target="_blank" rel="noopener noreferrer">Instagram</a>
+                <div>yakisuhshop@gmail.com</div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
