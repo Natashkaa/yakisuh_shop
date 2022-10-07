26cff1d334accee442349277b577c2f17d607a91
Natashkaa <natasha.cat982404@gmail.com>
2022-10-07 20:04:15 +0300

    Set goodtypes into header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -2,8 +2,24 @@ import "./header.css";
 import MenuIcon from "../../img/menu.png";
 import MainIcon from "../../img/hand-made.png";
 import ShopCard from "../../img/basket.png";
+import React, { useState } from "react";
 
-const Header = () => {
+const Header = (props) => {
+    const { getData } = props;
+    const [ items, setItems ] = useState([]);
+
+    getData().then(res => {
+        setItems(res.map((item) => {
+            const { typeId, typeName } = item;
+            return (
+                <li key={typeId}
+                    className="dropdown-item">{typeName}</li>
+            );
+        })
+        ); 
+    });
+
+
     return (
         <header className="header">
             <div className="menu-block">
@@ -11,12 +27,13 @@ const Header = () => {
                     <img className="icon" src={MenuIcon} alt="menu icon" />
                 </div>
                 <ul className="dropdown-menu">
-                    <li className="dropdown-item"><a href="#">Bath</a></li>
+                    {/* <li className="dropdown-item"><a href="#">Bath</a></li>
                     <li className="dropdown-item"><a href="#">Candles</a></li>
                     <li className="dropdown-item"><a href="#">Clay</a></li>
                     <li className="dropdown-item"><a href="#">Cosmetics</a></li>
                     <li className="dropdown-item"><a href="#">Clothes</a></li>
-                    <li className="dropdown-item"><a href="#">Jewelry</a></li>
+                    <li className="dropdown-item"><a href="#">Jewelry</a></li> */}
+                    {items}
                 </ul>
             </div>
 
